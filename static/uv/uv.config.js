b6f372be62b9d8a0cea298f62dd5c8a2f7ad601e
Geometry Tryhard <memelite69@gmail.com>
2025-09-01 14:37:03 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://erm.what-in.tf',
+    bare:'https://focus-bare-bay.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
