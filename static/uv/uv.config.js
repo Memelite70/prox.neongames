8816d5f501425099e5c46f865aab26f0928804b9
Geometry Tryhard <memelite69@gmail.com>
2025-09-01 15:09:15 -0400

    update

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://focus-bare-bay.vercel.app/',
+    bare:'https://focus-bare-dun.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
